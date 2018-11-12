89213ef700c3dc4ef180f53884a6afc8efd3f35b
DarthChurros <tnarayan00@stuy.edu>
2018-11-12 14:43:50 -0500

    added addWord, fixed toString

--- WordSearch.java
+++ WordSearch.java
@@ -1,5 +1,12 @@
+import java.util.*;
+import java.io.*;
+
 public class WordSearch {
   private char[][] data;
+  private int seed;
+  private Random randgen;
+  private ArrayList<String> wordsToAdd;
+  private ArrayList<String> wordsAdded;
 
   /**Initialize the grid to the size specified
    *and fill all of the positions with '_'
@@ -23,6 +30,79 @@ public class WordSearch {
     }
   }
 
+  /**Attempts to add a given word to the specified position of the WordGrid.
+   *The word is added in the direction rowIncrement,colIncrement
+   *Words must have a corresponding letter to match any letters that it overlaps.
+   *
+   *@param word is any text to be added to the word grid.
+   *@param row is the vertical locaiton of where you want the word to start.
+   *@param col is the horizontal location of where you want the word to start.
+   *@param rowIncrement is -1,0, or 1 and represents the displacement of each letter in the row direction
+   *@param colIncrement is -1,0, or 1 and represents the displacement of each letter in the col direction
+   *@return true when: the word is added successfully.
+   *        false when: the word doesn't fit, OR  rowIncrement and colIncrement are both 0,
+   *        OR there are overlapping letters that do not match
+   */
+   private boolean addWord(String word, int row, int col, int rowIncrement, int colIncrement) {
+     String reverse = "";
+     for (int i = word.length() - 1; i >= 0; i--) {
+       reverse += word.charAt(i);
+     }
+     if (rowIncrement == 1 && colIncrement == 1) {
+       return addWordDiagonal(word, row, col);
+     }
+     if (rowIncrement == 0 && colIncrement == 1) {
+       return addWordHorizontal(word, row, col);
+     }
+     if (rowIncrement == 1 && colIncrement == 0) {
+       return addWordVertical(word, row, col);
+     }
+     if (rowIncrement == -1 && colIncrement == 0) {
+       return addWordVertical(reverse, row, col - word.length() + 1);
+     }
+     if (rowIncrement == 0 && colIncrement == -1) {
+       return addWordHorizontal(reverse, row - word.length() + 1, col);
+     }
+     if (rowIncrement == -1 && colIncrement == -1) {
+       return addWordDiagonal(reverse, row - word.length() + 1, col - word.length() + 1);
+     }
+     if (rowIncrement == 1 && colIncrement == -1) {
+       if (row < 0 || row >= data.length || col < 0 || col >= data[0].length || word.length() >= data[0].length || word.length() >= data.length) {
+         return false;
+       }
+       for (int i = 0; i < word.length(); i++) {
+         if (row + i >= data.length || data[row+i][col] != '_' && data[row+i][col] != word.charAt(i)) {
+           return false;
+         }
+         if (col - i >= data[i].length || data[row][col-i] != '_' && data[row][col+i] != word.charAt(i)) {
+           return false;
+         }
+       }
+       for (int i = 0; i < word.length(); i++) {
+         data[row+i][col-i] = word.charAt(i);
+       }
+       return true;
+     }
+     if (rowIncrement == -1 && colIncrement == 1) {
+       if (row < 0 || row >= data.length || col < 0 || col >= data[0].length || word.length() >= data[0].length || word.length() >= data.length) {
+         return false;
+       }
+       for (int i = 0; i < word.length(); i++) {
+         if (row - i >= data.length || data[row-i][col] != '_' && data[row+i][col] != word.charAt(i)) {
+           return false;
+         }
+         if (col + i >= data[i].length || data[row][col+i] != '_' && data[row][col+i] != word.charAt(i)) {
+           return false;
+         }
+       }
+       for (int i = 0; i < word.length(); i++) {
+         data[row-i][col+i] = word.charAt(i);
+       }
+       return true;
+     }
+     return false;
+   }
+
   /**Each row is a new line, there is a space between each letter
    *@return a String with each character separated by spaces, and rows
    *separated by newlines.
@@ -30,10 +110,11 @@ public class WordSearch {
   public String toString() {
     String ans = "";
     for (int i = 0; i < data.length; i++) {
+      ans += "|";
       for (int j = 0; j < data[i].length; j++) {
         ans += data[i][j] + " ";
       }
-      ans += "\n";
+      ans += "\b|\n";
     }
     return ans;
   }
